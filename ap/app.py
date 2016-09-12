13e2fc4df354fc8a31696677f92a0a8fe0e091e2
yuvalsc <yuvalsc@gmail.com>
2016-09-12 22:12:42 +0300

    General apdates

--- ap/app.py
+++ ap/app.py
@@ -1,3 +1,4 @@
+# -*- coding: utf8 -*-
 """
 This sample demonstrates a simple skill built with the Amazon Alexa Skills Kit.
 The Intent Schema, Custom Slots, and Sample Utterances for this skill, as well
@@ -6,8 +7,11 @@ as testing instructions are located at https://github.com/yuvalsc/AmazonPrincipl
 This skill is maintained by Yuval Schwabe
 Created 10/Sep/2016
 """
-
 from __future__ import print_function
+from __future__ import unicode_literals
+import json
+import random
+
 
 def lambda_handler(event, context):
     """ Route the incoming request based on type (LaunchRequest, IntentRequest,
@@ -65,13 +69,13 @@ def on_intent(intent_request, session):
     intent_name = intent_request['intent']['name']
 
     # Dispatch to your skill's intent handlers
-    if intent_name == "MyColorIsIntent":
-        return set_color_in_session(intent, session)
-    elif intent_name == "WhatsMyColorIntent":
-        return get_color_from_session(intent, session)
+    if intent_name == "WhatsMyPricipleIntent" or intent_name == "AMAZON.YesIntent":
+        return get_principle_from_array(intent, session)
     elif intent_name == "AMAZON.HelpIntent":
         return get_welcome_response()
     elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+        return handle_session_end_request()
+    elif intent_name == "AMAZON.NoIntent":
         return handle_session_end_request()
     else:
         raise ValueError("Invalid intent")
@@ -96,13 +100,11 @@ def get_welcome_response():
 
     session_attributes = {}
     card_title = "Welcome"
-    speech_output = "Welcome to the Alexa Skills Kit sample. " \
-                    "Please tell me your favorite color by saying, " \
-                    "my favorite color is red"
+    speech_output = "Welcome to the amazon principles memory game. " \
+                    "Would you like to proceed? "
     # If the user either does not reply to the welcome message or says something
     # that is not understood, they will be prompted again with this text.
-    reprompt_text = "Please tell me your favorite color by saying, " \
-                    "my favorite color is red."
+    reprompt_text = "Would you like to proceed? "
     should_end_session = False
     return build_response(session_attributes, build_speechlet_response(
         card_title, speech_output, reprompt_text, should_end_session))
@@ -110,7 +112,7 @@ def get_welcome_response():
 
 def handle_session_end_request():
     card_title = "Session Ended"
-    speech_output = "Thank you for trying the Alexa Skills Kit sample. " \
+    speech_output = "Thank you for playing amazon principles memory game. " \
                     "Have a nice day! "
     # Setting this to true ends the session and exits the skill.
     should_end_session = True
@@ -118,7 +120,7 @@ def handle_session_end_request():
         card_title, speech_output, None, should_end_session))
 
 
-def set_color_in_session(intent, session):
+def check_principle_is_correct(intent, session):
     """ Sets the color in the session and prepares the speech to reply to the
     user.
     """
@@ -127,9 +129,9 @@ def set_color_in_session(intent, session):
     session_attributes = {}
     should_end_session = False
 
-    if 'Color' in intent['slots']:
-        favorite_color = intent['slots']['Color']['value']
-        session_attributes = create_favorite_color_attributes(favorite_color)
+    if 'Principle' in intent['slots']:
+        principle_name = intent['slots']['Principle']['value']
+        session_attributes = create_principle_number_attributes(principle_number)
         speech_output = "I now know your favorite color is " + \
                         favorite_color + \
                         ". You can ask me your favorite color by saying, " \
@@ -146,15 +148,32 @@ def set_color_in_session(intent, session):
         card_title, speech_output, reprompt_text, should_end_session))
 
 
-def create_favorite_color_attributes(favorite_color):
-    return {"favoriteColor": favorite_color}
+def create_principle_number_attributes(principle_number):
+    return {"lastPrincipleSelected": principle_number}
 
+def create_principles_array():
+    return {
+        "Customer Obsession": "Leaders start with the customer and work backwards. They work vigorously to earn and keep customer trust. Although leaders pay attention to competitors, they obsess over customers.",
+        "Ownership": "Leaders are owners. They think long term and don’t sacrifice long-term value for short-term results. They act on behalf of the entire company, beyond just their own team. They never say ""that’s not my job"".",
+        "Invent and Simplify": "Leaders expect and require innovation and invention from their teams and always find ways to simplify. They are externally aware, look for new ideas from everywhere, and are not limited by ""not invented here"". As we do new things, we accept that we may be misunderstood for long periods of time.",
+        "Are Right, A Lot": "Leaders are right a lot. They have strong judgment and good instincts. They seek diverse perspectives and work to disconfirm their beliefs.",
+        "Learn and Be Curious": "Leaders are never done learning and always seek to improve themselves. They are curious about new possibilities and act to explore them.",
+        "Hire and Develop The Best": "Leaders raise the performance bar with every hire and promotion. They recognize exceptional talent, and willingly move them throughout the organization. Leaders develop leaders and take seriously their role in coaching others.  We work on behalf of our people to invent mechanisms for development like Career Choice.",
+        "Insist on the Highest Standards": "Leaders have relentlessly high standards - many people may think these standards are unreasonably high. Leaders are continually raising the bar and driving their teams to deliver high quality products, services and processes. Leaders ensure that defects do not get sent down the line and that problems are fixed so they stay fixed.",
+        "Think Big": "Thinking small is a self-fulfilling prophecy. Leaders create and communicate a bold direction that inspires results. They think differently and look around corners for ways to serve customers.",
+        "Bias for Action": "Speed matters in business. Many decisions and actions are reversible and do not need extensive study. We value calculated risk taking. ",
+        "Frugality": "Accomplish more with less. Constraints breed resourcefulness, self-sufficiency and invention.  There are no extra points for growing headcount, budget size or fixed expense.",
+        "Earn Trust": "Leaders listen attentively, speak candidly, and treat others respectfully. They are vocally self-critical, even when doing so is awkward or embarrassing.  Leaders do not believe their or their team’s body odor smells of perfume.  They benchmark themselves and their teams against the best.",
+        "Dive Deep": "Leaders operate at all levels, stay connected to the details, audit frequently, and are skeptical when metrics and anecdote differ. No task is beneath them.",
+        "Have Backbone Disagree and Commit": "Leaders are obligated to respectfully challenge decisions when they disagree, even when doing so is uncomfortable or exhausting. Leaders have conviction and are tenacious. They do not compromise for the sake of social cohesion. Once a decision is determined, they commit wholly.",
+        "Deliver Results": "Leaders focus on the key inputs for their business and deliver them with the right quality and in a timely fashion. Despite setbacks, they rise to the occasion and never settle."
+    }
 
-def get_color_from_session(intent, session):
+def get_color_from_sessionn(intent, session):
     session_attributes = {}
     reprompt_text = None
 
-    if session.get('attributes', {}) and "favoriteColor" in session.get('attributes', {}):
+    if session.get('attributes', {}) and "lastPrincipleSelected" in session.get('attributes', {}):
         favorite_color = session['attributes']['favoriteColor']
         speech_output = "Your favorite color is " + favorite_color + \
                         ". Goodbye."
@@ -162,6 +181,30 @@ def get_color_from_session(intent, session):
     else:
         speech_output = "I'm not sure what your favorite color is. " \
                         "You can say, my favorite color is red."
+        should_end_session = False
+
+    # Setting reprompt_text to None signifies that we do not want to reprompt
+    # the user. If the user does not respond or says something that is not
+    # understood, the session will end.
+    return build_response(session_attributes, build_speechlet_response(
+        intent['name'], speech_output, reprompt_text, should_end_session))
+
+def get_principle_from_array(intent, session):
+    session_attributes = {}
+    reprompt_text = None
+
+    jsonObject = create_principles_array()
+
+    if session.get('attributes', {}) and "lastPrincipleSelected" in session.get('attributes', {}):
+        principle_number = session['attributes']['lastPrincipleSelected']
+        if 'Principle' in intent['slots']:
+            principle_name = intent['slots']['Principle']['value']
+            speech_output = "You said " + principle_name + ", and principle number in session is " + str(principle_number)
+            should_end_session = True
+    else:
+        principle_number = random.randint(1,14)
+        session_attributes = create_principle_number_attributes(principle_number)
+        speech_output = "What is principle number " + str(principle_number)
         should_end_session = False
 
     # Setting reprompt_text to None signifies that we do not want to reprompt
